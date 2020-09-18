6f33c41390905bab0949fc8ffb173cb341734ab7
chinweokwu <paulcmorah@gmail.com>
2020-09-18 02:46:22 +0100

    added event attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :creator, :class_name => "User"
+    has_many :attendees, 		:through => :event_attendees
+    has_many :event_attendees,  :foreign_key => :attended_event_id
 
     validates :name, presence: true
     validates :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :events, :foreign_key => :creator_id
+    has_many :attended_events,  :through => :event_attendees
+    has_many :event_attendees,  :foreign_key => :attendee_id
 
     EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :username, presence: true, uniqueness: true
