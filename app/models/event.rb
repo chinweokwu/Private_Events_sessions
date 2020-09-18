acbca1e95378d8443ed72eb790289733857a1d03
chinweokwu <paulcmorah@gmail.com>
2020-09-18 02:03:58 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+    belongs_to :creator, :class_name => "User"
+
+    validates :name, presence: true
+    validates :location, presence: true
+    validates :description, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :events, :foreign_key => :creator_id
+
+    EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, format: { with: EMAIL_FORMAT_REGEX }, uniqueness: true
+end
