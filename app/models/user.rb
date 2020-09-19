ed448ba68fe9b2ca31e95709662ac652bced6010
chinweokwu <paulcmorah@gmail.com>
2020-09-19 02:45:44 +0100

    added attending event to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # :nodoc:
 class User < ApplicationRecord
+  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
   has_many :events, dependent: :destroy, foreign_key: :creator_id
-  has_many :attended_events, through: :event_attendees
   has_many :event_attendees, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendees
+
 
   EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :username, presence: true, uniqueness: true
@@ -18,21 +20,5 @@ class User < ApplicationRecord
 
   def cancel!(event)
     event_attendees.find_by(attended_event_id: event.id).destroy
-  end
-
-  def future_events
-    attended_events.future
-  end
-
-  def past_events
-    attended_events.past
-  end
-
-  def future_attended_events(_user)
-    @attended_events.future
-  end
-
-  def current_user?(user)
-    current_user == user
   end
 end
