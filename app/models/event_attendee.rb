991d427837fd60ecf912c3bbe536ea3b1a5cc536
Laguna1 <lagunazoomarket@gmail.com>
2020-09-21 15:08:38 +0300

    check linter errors

--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -1,6 +1,6 @@
 # :nodoc:
 class EventAttendee < ApplicationRecord
-  belongs_to :attendee,	class_name: 'User'
+  belongs_to :attendee, class_name: 'User'
   belongs_to :attended_event, class_name: 'Event'
 
   validates :attendee_id,	presence: true
